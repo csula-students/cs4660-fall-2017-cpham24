f28fe2cd2a6c5592e54b6416cdb3981d4c548ef5
Bill <billvpham@gmail.com>
2017-09-02 10:48:02 -0700

    finished python exercise

--- cs4660/tutorial/files.py
+++ cs4660/tutorial/files.py
@@ -1,38 +1,48 @@
 """Files tests simple file read related operations"""
+from io import open
+import lists
 
 class SimpleFile(object):
     """SimpleFile tests using file read api to do some simple math"""
     def __init__(self, file_path):
+        f = open(file_path, encoding='utf-8')
+        content = f.readlines()
         self.numbers = []
-        """
-        TODO: reads the file by path and parse content into two
-        dimension array (numbers)
-        """
+        index = 0
+        for line in content:
+          entries = line.split(' ')
+          li = []
+          for entry in entries:
+            try:
+              x = int(entry)
+              li.append(x)
+            except ValueError:
+              """TODO: handle error"""
+          self.numbers.append(li)
+          index += 1
 
     def get_mean(self, line_number):
-        """
-        get_mean retrieves the mean value of the list by line_number (starts
-        with zero)
-        """
-        pass
+        return lists.get_avg(self.numbers[line_number])
 
     def get_max(self, line_number):
-        """
-        get_max retrieves the maximum value of the list by line_number (starts
-        with zero)
-        """
-        pass
+        list = self.numbers[line_number]
+        max = list[0]
+        for num in list:
+          if num > max:
+            max = num
+        return max
 
     def get_min(self, line_number):
-        """
-        get_min retrieves the minimum value of the list by line_number (starts
-        with zero)
-        """
-        pass
+        list = self.numbers[line_number]
+        min = list[0]
+        for num in list:
+          if num < min:
+            min = num
+        return min
 
     def get_sum(self, line_number):
-        """
-        get_sum retrieves the sumation of the list by line_number (starts with
-        zero)
-        """
-        pass
+        list = self.numbers[line_number]
+        sum = 0
+        for num in list:
+          sum += num
+        return sum
--- cs4660/tutorial/lists.py
+++ cs4660/tutorial/lists.py
@@ -1,21 +1,22 @@
 """Lists defines simple list related operations"""
 
 def get_first_item(li):
-    """Return the first item from the list"""
-    pass
+    return li[0]
 
 def get_last_item(li):
-    """Return the last item from the list"""
-    pass
+    size = len(li)
+    return li[size-1]
 
 def get_second_and_third_items(li):
-    """Return second and third item from the list"""
-    pass
+    return [li[1], li[2]]
 
 def get_sum(li):
-    """Return the sum of the list items"""
-    pass
+    sum = 0
+    for num in li:
+        sum += num
+    return sum
 
 def get_avg(li):
-    """Returns the average of the list items"""
-    pass
+    size = len(li)
+    sum = float(get_sum(li))
+    return sum / size
