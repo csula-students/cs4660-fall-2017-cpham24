79c3dfafcf960e2065505a571c74291666993b5a
Bill <billvpham@gmail.com>
2017-09-16 09:39:25 -0700

    recovered deleted files

---
+++ cs4660/tutorial/files.py
@@ -0,0 +1,36 @@
+"""Files tests simple file read related operations"""
+from io import open
+from tutorial import lists
+
+class SimpleFile(object):
+  """SimpleFile tests using file read api to do some simple math"""
+  def __init__(self, file_path):
+    f = open(file_path, encoding='utf-8')
+    content = f.readlines()
+    self.numbers = []
+    for line in content:
+      entries = line.split(' ')
+      li = []
+      for entry in entries:
+        try:
+          x = int(entry)
+          li.append(x)
+        except ValueError:
+          """TODO: handle error"""
+      self.numbers.append(li)
+
+  def get_mean(self, line_number):
+    list = self.numbers[line_number]
+    return lists.get_avg(list)
+
+  def get_max(self, line_number):
+    list = self.numbers[line_number]
+    return max(list)
+
+  def get_min(self, line_number):
+    list = self.numbers[line_number]
+    return min(list)
+
+  def get_sum(self, line_number):
+    list = self.numbers[line_number]
+    return sum(list)
