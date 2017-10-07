f234a572a01f78c98c91afd7155c95db6a76d761
Bill <billvpham@gmail.com>
2017-10-07 08:45:47 -0700

    recovered past exercises and homeworks

--- cs4660/tutorial/files.py
+++ cs4660/tutorial/files.py
@@ -1,38 +1,36 @@
 """Files tests simple file read related operations"""
+from io import open
+from tutorial import lists
 
 class SimpleFile(object):
-    """SimpleFile tests using file read api to do some simple math"""
-    def __init__(self, file_path):
-        self.numbers = []
-        """
-        TODO: reads the file by path and parse content into two
-        dimension array (numbers)
-        """
+  """SimpleFile tests using file read api to do some simple math"""
+  def __init__(self, file_path):
+    f = open(file_path, encoding='utf-8')
+    content = f.readlines()
+    self.numbers = []
+    for line in content:
+      entries = line.split(' ')
+      li = []
+      for entry in entries:
+        try:
+          x = int(entry)
+          li.append(x)
+        except ValueError:
+          """TODO: handle error"""
+      self.numbers.append(li)
 
-    def get_mean(self, line_number):
-        """
-        get_mean retrieves the mean value of the list by line_number (starts
-        with zero)
-        """
-        pass
+  def get_mean(self, line_number):
+    list = self.numbers[line_number]
+    return lists.get_avg(list)
 
-    def get_max(self, line_number):
-        """
-        get_max retrieves the maximum value of the list by line_number (starts
-        with zero)
-        """
-        pass
+  def get_max(self, line_number):
+    list = self.numbers[line_number]
+    return max(list)
 
-    def get_min(self, line_number):
-        """
-        get_min retrieves the minimum value of the list by line_number (starts
-        with zero)
-        """
-        pass
+  def get_min(self, line_number):
+    list = self.numbers[line_number]
+    return min(list)
 
-    def get_sum(self, line_number):
-        """
-        get_sum retrieves the sumation of the list by line_number (starts with
-        zero)
-        """
-        pass
+  def get_sum(self, line_number):
+    list = self.numbers[line_number]
+    return sum(list)
